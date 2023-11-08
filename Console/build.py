df4632f39905c0e3aee491d74d08795089b8f659
Mark Qvist <mark@unsigned.io>
2023-11-08 20:47:12 +0100

    Updated console package version

--- Console/build.py
+++ Console/build.py
@@ -4,7 +4,7 @@ import sys
 import shutil
 
 packages = {
-    "rns": "rns-0.6.5-py3-none-any.whl",
+    "rns": "rns-0.6.7-py3-none-any.whl",
     "nomadnet": "nomadnet-0.4.2-py3-none-any.whl",
     "lxmf": "lxmf-0.3.8-py3-none-any.whl",
     "rnsh": "rnsh-0.1.2-py3-none-any.whl",
