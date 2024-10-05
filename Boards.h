e4deac30606c70d94ccab55c324aa651788e3b60
liamcottle <liam@liamcottle.com>
2024-10-05 19:12:26 +1300

    add support for user button on rak wismesh pocket which uses RAK4630 internally

--- Boards.h
+++ Boards.h
@@ -569,6 +569,7 @@
       #define HAS_TCXO true
       #define HAS_RF_SWITCH_RX_TX true
       #define HAS_BUSY true
+      #define HAS_INPUT true
       #define DIO2_AS_RF_SWITCH true
       #define CONFIG_UART_BUFFER_SIZE 6144
       #define CONFIG_QUEUE_SIZE 6144
@@ -577,6 +578,8 @@
       #define EEPROM_OFFSET EEPROM_SIZE-EEPROM_RESERVED
       #define BLE_MANUFACTURER "RAK Wireless"
       #define BLE_MODEL "RAK4640"
+
+      const int pin_btn_usr1 = 9;
 
       // Following pins are for the sx1262
       const int pin_rxen = 37;
